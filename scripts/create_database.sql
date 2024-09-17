347938d7bab664e65e5da871c86cb3f4bb4d12f4
Azeem Sajid <azeem.sajid@gmail.com>
2024-09-17 11:16:27 +0500

    Minor cleanup

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -1,17 +1,12 @@
 ALTER SESSION SET CONTAINER = FREEPDB1;
 CREATE USER myUser IDENTIFIED BY myPwd QUOTA UNLIMITED ON USERS;
--- CREATE SCHEMA AUTHORIZATION myUser;
 
 GRANT CONNECT, RESOURCE TO myUser;
 GRANT CREATE TABLE TO myUser;
 ALTER SESSION SET CURRENT_SCHEMA = myUser;
 
-SELECT * FROM all_tab_privs_recd WHERE grantee = 'myUser';
-
 CREATE TABLE FESTIVO (
   FECHA   DATE    NOT NULL
 );
 
--- SELECT table_name FROM all_tables;
--- SELECT table_name FROM all_tables WHERE OWNER = 'myUser';
- Select owner from dba_tables where table_name = 'FESTIVO'; 
+SELECT OWNER FROM dba_tables WHERE table_name = 'FESTIVO';
